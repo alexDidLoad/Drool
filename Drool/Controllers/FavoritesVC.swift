b90258e20eb4093bb1ec38b5e56d1e8a5a4e3164
Alexander Ha <Ha.alex.h@gmail.com>
2021-01-09 14:19:50 -0800

    Initial Commit

---
+++ Drool/Controllers/FavoritesVC.swift
@@ -0,0 +1,27 @@
+//
+//  FavoritesVC.swift
+//  Drool
+//
+//  Created by Alexander Ha on 1/9/21.
+//
+
+import UIKit
+
+class FavoritesVC: UIViewController {
+    
+    //MARK: - UIComponents
+    
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+    }
+    
+    //MARK: - Helpers
+    
+    
+}
---
+++ Drool/Controllers/HomeVC.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Drool
+//
+//  Created by Alexander Ha on 1/9/21.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    //MARK: - UIComponents
+    
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+    
+    //MARK: - Helpers
+    
+
+
+}
+
